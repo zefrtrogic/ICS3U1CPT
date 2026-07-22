767fc3bc317b805a06e8eb39d2c2a96ecedc3b20
zefrtrogic <eding2960@gmail.com>
2026-07-22 11:09:33 -0400

    Logo fix

--- ICS3U1CPT/src/main/Main.java
+++ ICS3U1CPT/src/main/Main.java
@@ -1,23 +1,30 @@
 package main;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import java.net.URL;
 import java.io.*;
+
 public class Main {
 	public static void main(String[] args) throws IOException {
-		//Creating the Window
-		JFrame window = new JFrame(); //creating window
-		window.setTitle("Pokemon"); //naming game
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //closes when the user closes the program
-		ImageIcon logo = new ImageIcon("pokemon.png"); //making variable to store logo
-		//Finding Dimensions and scaling
-		GamePanel gamepanel = new GamePanel(); //Running gamepanel class to get dimension of the screen
-		window.add(gamepanel); //adding the dimension of the gamepanel to the JFrame
-		window.pack(); //fits the screen to the preffered size of the window
-		//Creating Logo
-		window.setIconImage(logo.getImage()); //Displaying logo
-		window.setLocationRelativeTo(null); //Displaying window at center of screen
-		window.setVisible(true); //allows user to see the window
-		//Calling and stating the game from the gamepanel class and startgamethread method
+		JFrame window = new JFrame();
+		window.setTitle("Pokemon");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		//Load the logo through the classpath instead of a raw file path
+		URL logoURL = Main.class.getResource("/logo/pokemon.png");
+		if (logoURL == null) {
+			System.out.println("Logo not found! Check that res is marked as a source/resources folder.");
+		} else {
+			ImageIcon logo = new ImageIcon(logoURL);
+			window.setIconImage(logo.getImage());
+		}
+
+		GamePanel gamepanel = new GamePanel();
+		window.add(gamepanel);
+		window.pack();
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
+
 		gamepanel.startGameThread();
 	}
 }
